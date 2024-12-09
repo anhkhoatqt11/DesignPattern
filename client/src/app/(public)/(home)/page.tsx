6a9ad90d336039061e3aaf2609db1421ecc33c07
Trương Anh Khoa <anhkhoatqt11@gmail.com>
2024-12-09 21:52:15 +0700

    fix: Fix session define, remove "use client" from home

--- client/src/app/(public)/(home)/page.tsx
+++ client/src/app/(public)/(home)/page.tsx
@@ -1,12 +1,10 @@
-"use client";
-
 import React from "react";
 import BannerCarousel from "./components/BannerCarousel";
 import ListComponent from "./components/ListComponent";
 import { getSession } from "@/lib/auth";
 
 const page = async () => {
-  const Session = await getSession();
+  const session = await getSession();
   return (
     <div className="bg-[#141414]">
       <BannerCarousel />
