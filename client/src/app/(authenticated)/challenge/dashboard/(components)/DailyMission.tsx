9842d4807471cdc8e7b56b9d7a73a6a9e7157d97
Son Dang <son.dang@devsamurai.com>
2024-12-21 17:54:09 +0700

    Change ui daily quest

--- client/src/app/(authenticated)/challenge/dashboard/(components)/DailyMission.tsx
+++ client/src/app/(authenticated)/challenge/dashboard/(components)/DailyMission.tsx
@@ -1,67 +1,86 @@
-import React from 'react'
-import { Image } from '@nextui-org/react'
-import toast from 'react-hot-toast'
+import React from "react";
+import { Image } from "@nextui-org/react";
+import toast from "react-hot-toast";
 
-const DailyMission = ({ currentDateTime, hasReceivedDailyGift, updateLoginLog, userId, refetchUserCoinAndQCData }) => {
-    const daysOfWeek = ['Chủ nhật', 'Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7']
+const DailyMission = ({
+  currentDateTime,
+  hasReceivedDailyGift,
+  updateLoginLog,
+  userId,
+  refetchUserCoinAndQCData,
+  loginGift,
+}) => {
+  const daysOfWeek = [
+    "Chủ nhật",
+    "Thứ 2",
+    "Thứ 3",
+    "Thứ 4",
+    "Thứ 5",
+    "Thứ 6",
+    "Thứ 7",
+  ];
 
-    const date = new Date(currentDateTime)
-    const currentDayIndex = date.getDay()
+  const date = new Date(currentDateTime);
+  const currentDayIndex = date.getDay();
 
-    const handleDayClick = async () => {
-        if (hasReceivedDailyGift) {
-            toast.success('Bạn đã nhận quà hôm nay rồi!');
-            return;
-        }; // Prevent clicking if gift already received
+  const handleDayClick = async () => {
+    if (hasReceivedDailyGift) {
+      toast.success("Bạn đã nhận quà hôm nay rồi!");
+      return;
+    } // Prevent clicking if gift already received
 
-        const formData = { userId };
+    const formData = { userId };
 
-        try {
-            // Call updateLoginLog with the userId
-            await updateLoginLog(formData);
-            console.log('Login log updated successfully.');
-            toast.success('Nhận quà thành công!');
+    try {
+      // Call updateLoginLog with the userId
+      await updateLoginLog(formData);
+      console.log("Login log updated successfully.");
+      toast.success("Nhận quà thành công!");
 
-            // Update the session
-            refetchUserCoinAndQCData();
-            console.log('Session updated successfully.');
-        } catch (error) {
-            console.error('Error updating login log or session:', error);
-        }
-    };
+      // Update the session
+      refetchUserCoinAndQCData();
+      console.log("Session updated successfully.");
+    } catch (error) {
+      console.error("Error updating login log or session:", error);
+    }
+  };
 
-    return (
-        <div>
-            <h2 className="text-2xl font-semibold mb-4 text-purple-300">Nhiệm vụ hàng ngày</h2>
-            <div className="grid grid-cols-7 gap-2 mb-6">
-                {daysOfWeek.map((day, index) => {
-                    // Determine background color based on current day and gift status
-                    let bgColor = 'bg-neutral-700'
-                    let cursorStyle = ''
-                    if (index === currentDayIndex) {
-                        bgColor = hasReceivedDailyGift ? 'bg-green-500' : 'bg-purple-500'
-                        cursorStyle = 'cursor-pointer'
-                    } else if (index < currentDayIndex) {
-                        bgColor = 'bg-neutral-500'
-                    } else if (index > currentDayIndex) {
-                        bgColor = 'bg-neutral-800'
-                    }
+  return (
+    <div>
+      <h2 className="text-xl font-semibold mb-4 text-white">
+        Nhiệm vụ hàng ngày
+      </h2>
+      <div className="grid grid-cols-7 gap-2 mb-6">
+        {daysOfWeek.map((day, index) => {
+          // Determine background color based on current day and gift status
+          let bgColor = "bg-[#4A4A4A]";
+          let cursorStyle = "";
+          if (index === currentDayIndex) {
+            bgColor = hasReceivedDailyGift ? "bg-[#4A4A4A]" : "bg-[#DA5EF0]";
+            cursorStyle = "cursor-pointer";
+          } else if (index < currentDayIndex) {
+            bgColor = "bg-[#4A4A4A]";
+          } else if (index > currentDayIndex) {
+            bgColor = "bg-[#2A2A2F]";
+          }
 
-                    return (
-                        <div
-                            key={day}
-                            className={`flex flex-col items-center rounded-lg p-2 space-y-2 ${bgColor} ${cursorStyle}`}
-                            onClick={index === currentDayIndex ? handleDayClick : undefined} // Add click handler only for the current day
-                        >
-                            <span className="text-xs text-gray-300 font-bold">{day}</span>
-                            <Image src="/skycoin.png" alt="star" width={50} height={50} />
-                            <span className="text-xs text-yellow-400">x10</span>
-                        </div>
-                    )
-                })}
+          return (
+            <div
+              key={day}
+              className={`flex flex-col items-center rounded-lg p-2 space-y-1 ${bgColor} ${cursorStyle}`}
+              onClick={index === currentDayIndex ? handleDayClick : undefined} // Add click handler only for the current day
+            >
+              <span className="text-xs text-gray-300 font-bold text-center">
+                {day}
+              </span>
+              <Image src="/skycoin.png" alt="star" width={25} height={25} />
+              <span className="text-xs text-[#FF9100]">x{loginGift}</span>
             </div>
-        </div>
-    )
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default DailyMission
+export default DailyMission;
--- client/src/app/(authenticated)/challenge/dashboard/(components)/DailyQuest.tsx
+++ client/src/app/(authenticated)/challenge/dashboard/(components)/DailyQuest.tsx
@@ -1,93 +1,117 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
-import toast from 'react-hot-toast'
+import toast from "react-hot-toast";
+import { Image } from "@nextui-org/react";
+import { CircularProgress } from "@nextui-org/react";
 
-
-const DailyQuest = ({ dailyQuests, questLog, updateQuestLog, userId, refetchUserCoinAndQCData }) => {
-    const handleClaimQuest = async (quest) => {
-        const userInfo = {
-            id: userId, // Replace with actual user ID
-            questLog,
-        };
-        try {
-            console.log(quest._id, userInfo);
-            await updateQuestLog(quest._id, userInfo);
-            toast.success("Đã nhận quà hoàn thành nhiệm vụ");
-            refetchUserCoinAndQCData();
-        } catch (error) {
-            console.error("Error claiming quest:", error);
-            toast.error("Không thể nhận quà");
-        }
+const DailyQuest = ({
+  dailyQuests,
+  questLog,
+  updateQuestLog,
+  userId,
+  refetchUserCoinAndQCData,
+}) => {
+  const handleClaimQuest = async (quest) => {
+    const userInfo = {
+      id: userId, // Replace with actual user ID
+      questLog,
     };
+    try {
+      console.log(quest._id, userInfo);
+      await updateQuestLog(quest._id, userInfo);
+      toast.success("Đã nhận quà hoàn thành nhiệm vụ");
+      refetchUserCoinAndQCData();
+    } catch (error) {
+      console.error("Error claiming quest:", error);
+      toast.error("Không thể nhận quà");
+    }
+  };
 
-    return (
-        <div className="space-y-6 rounded-lg shadow-lg">
-            <div className="space-y-4">
-                {dailyQuests.map((quest) => {
-                    const isReadingQuest = quest.questType === "reading";
-                    const isWatchingQuest = quest.questType === "watching";
+  return (
+    <div className="space-y-6 rounded-lg shadow-lg">
+      <div className="space-y-4">
+        {dailyQuests.map((quest) => {
+          if (quest.questType === "login") return;
+          const isReadingQuest = quest.questType === "reading";
+          const isWatchingQuest = quest.questType === "watching";
 
-                    const userProgress = isReadingQuest
-                        ? questLog.readingTime
-                        : isWatchingQuest
-                            ? questLog.watchingTime
-                            : 0;
+          const userProgress = isReadingQuest
+            ? questLog.readingTime
+            : isWatchingQuest
+            ? questLog.watchingTime
+            : 0;
 
-                    const isCompleted = userProgress >= quest.requiredTime;
-                    const hasReceived = questLog.received.some((id) => id.$oid === quest._id);
+          const isCompleted = userProgress >= quest.requiredTime;
+          const hasReceived = questLog.received.some(
+            (id) => id.$oid === quest._id
+          );
 
-                    const progressPercentage = Math.min(
-                        (userProgress / quest.requiredTime) * 100,
-                        100
-                    ).toFixed(0);
+          const progressPercentage = Math.round(
+            Math.min((userProgress / quest.requiredTime) * 100, 100)
+          );
 
-                    let buttonText = "Nhận";
-                    let isDisabled = false;
+          let buttonText = "Nhận";
+          let isDisabled = false;
 
-                    if (hasReceived) {
-                        buttonText = "Đã nhận";
-                        isDisabled = true;
-                    } else if (!isCompleted) {
-                        buttonText = "Chưa hoàn thành";
-                        isDisabled = true;
-                    }
+          if (hasReceived) {
+            buttonText = "Đã nhận";
+            isDisabled = true;
+          } else if (!isCompleted) {
+            buttonText = "Chưa hoàn thành";
+            isDisabled = true;
+          }
 
-                    return (
-                        <div
-                            key={quest._id}
-                            className="p-4 bg-neutral-800 rounded-lg flex flex-col shadow-md space-y-4"
-                        >
-                            <div>
-                                <h3 className="text-white font-semibold text-lg mb-1">{quest.questName}</h3>
-                                <p className="text-sm text-gray-400 mb-2">
-                                    Phần thưởng: <span className="text-yellow-400">{quest.prize} xu</span>
-                                </p>
-                                <p className="text-sm text-gray-400 mb-2">
-                                    Yêu cầu: {quest.requiredTime > 0 ? `${quest.requiredTime} tập` : "Không yêu cầu"}
-                                </p>
-                                <p className="text-sm text-gray-400 mb-2">
-                                    Tiến độ: {userProgress} / {quest.requiredTime} tập ({progressPercentage}%)
-                                </p>
-                                <div className="w-full bg-gray-600 rounded-full h-2.5 overflow-hidden">
-                                    <div
-                                        className="bg-green-500 h-2.5 rounded-full"
-                                        style={{ width: `${progressPercentage}%` }}
-                                    ></div>
-                                </div>
-                            </div>
-                            <Button
-                                className="ml-auto"
-                                disabled={isDisabled}
-                                onClick={() => handleClaimQuest(quest)}
-                            >
-                                {buttonText}
-                            </Button>
-                        </div>
-                    );
-                })}
+          return (
+            <div
+              key={quest._id}
+              className="p-3 px-5 bg-neutral-800 rounded-lg flex flex-col shadow-md space-y-2"
+            >
+              <div>
+                <div className="flex flex-row w-full justify-between items-center">
+                  <h3 className="text-white font-semibold text-lg mb-1">
+                    {quest.questName}
+                  </h3>
+                  <CircularProgress
+                    classNames={{
+                      indicator: "stroke-[#A958FE]",
+                      track: "stroke-white/10",
+                    }}
+                    aria-label="Loading..."
+                    color="success"
+                    showValueLabel={true}
+                    size="lg"
+                    value={progressPercentage}
+                  />
+                </div>
+              </div>
+              <div className="w-full flex flex-row justify-between items-end">
+                <p className="text-sm text-gray-400 mb-2">
+                  <div className="flex flex-row gap-1 items-end">
+                    <Image
+                      src="/skycoin.png"
+                      alt="star"
+                      width={40}
+                      height={40}
+                    />
+                    <span className="text-yellow-400 text-lg font-medium">
+                      x{quest.prize}
+                    </span>
+                  </div>
+                </p>
+                <Button
+                  className="py-0 px-3"
+                  disabled={isDisabled}
+                  onClick={() => handleClaimQuest(quest)}
+                >
+                  {buttonText}
+                </Button>
+              </div>
             </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default DailyQuest;
