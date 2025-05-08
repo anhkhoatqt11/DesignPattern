86d69d977c51e75878ce224544261ec5fb4a0734
Trinh Minh Quan <121121741+q113pt@users.noreply.github.com>
2025-05-08 19:55:02 +0700

    Revert "apply singleton"
    This reverts commit 5920caf7c9a37aea85bd8f05bbdf10b4502fb68c.

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,18 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-// Add your reducers here
-const reducer = {
-  // Add reducers here, e.g., auth: authReducer
-};
+const store = configureStore({
+  reducer: {
+    // Add other reducers if needed
+  },
+});
 
-let store: ReturnType<typeof configureStore> | null = null;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
-export function getStore() {
-  if (!store) {
-    store = configureStore({ reducer });
-  }
-  return store;
-}
-
-export type RootState = ReturnType<ReturnType<typeof getStore>["getState"]>;
-export type AppDispatch = ReturnType<typeof getStore>["dispatch"];
+export default store;
